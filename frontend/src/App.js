585e63de19d1b495a1a48e4e24e6e0ca9b076fac
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2023-02-06 16:59:49 -0300

    quase pronto!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import http from './http';
+const parse = require('html-react-parser');
 
 function App() {
   const [data, setData] = useState();
@@ -9,10 +10,6 @@ function App() {
   const [password, setPassword] = useState("");
   const [atual, setAtual] = useState(true);
   const controle = window.location.pathname == "/controle";
-
-
-  const atualizar = () => {
-  }
 
   useEffect(() => {
     const interval = setInterval(() => setTime(Date.now()), 5000);
@@ -38,8 +35,8 @@ function App() {
   useEffect(() => {
     if (controle) {
       http.post('/alterar', {
-          pagina: pagina.toString(),
-          senha: password,
+        pagina: pagina.toString(),
+        senha: password,
       })
     }
 
@@ -51,29 +48,41 @@ function App() {
   return (
     <div className="App">
       <h3>{data.data.titulo}</h3>
+      <p>{data.data.referencia}</p>
       <table>
         <tbody>
+          <tr hidden={data.data.partitura == ""}>
+            <td colSpan={4}>
+              <img src={'imagens/' + data.data.partitura} />
+
+            </td>
+          </tr>
           <tr>
-            <td colSpan={2} style={{ width: '50%' }}>{data.data.portugues}</td>
-            <td colSpan={2} style={{ width: '50%' }}>{data.data.latim}</td>
+            <td colSpan={2} style={{ width: '50%' }}>{parse(data.data.portugues)}</td>
+            <td colSpan={2} style={{ width: '50%' }}>{parse(data.data.latim)}</td>
           </tr>
 
+
+        </tbody>
+
+      </table>
+      <table className='footer'>
+        <tbody>
           <tr>
             <td><input type="button" value={"Anterior"} onClick={() => {
-              setPagina(parseInt(pagina) - 1).then(() => atualizar());
+              setPagina(parseInt(pagina) - 1);
             }} disabled={atual || pagina == 1} /></td>
             <td><input type="button" value={atual ? "Sincronizado" : "Não sincronizado!"} onClick={() => {
-              setAtual(!atual).then(() => atualizar());
+              setAtual(!atual);
             }} /></td>
             <td><input type="button" value={"Próximo"} onClick={() => {
-              setPagina(parseInt(pagina) + 1).then(() => atualizar());
+              setPagina(parseInt(pagina) + 1);
             }} disabled={atual} /></td>
           </tr>
           <tr>
             <td colSpan={4}>Página {pagina}</td>
           </tr>
         </tbody>
-
         {/* CONTROLES ESPECIAIS */}
         <tfoot hidden={!controle}>
           <tr>
